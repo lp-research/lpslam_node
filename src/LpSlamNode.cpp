a35afac8af269990b0390864eb458823b6bb27ed
Thomas Hauth <thomas.hauth@web.de>
2021-05-19 15:51:02 +0900

    added missing include

--- src/LpSlamNode.cpp
+++ src/LpSlamNode.cpp
@@ -32,6 +32,7 @@
 #include <algorithm>
 #include <chrono>
 
+#include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/create_timer_ros.h>
